77135d9e2518e76e868a0b3aa4b57015736374d2
Alex Horban <andromeda@Alexs-Mac-mini.local>
2024-10-22 01:18:25 -0400

    trying to fix styling / import issue

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -13,3 +13,4 @@ from .autodiff import *  # noqa: F401,F403
 from .scalar import *  # noqa: F401,F403
 from .scalar_functions import *  # noqa: F401,F403
 from .module import *  # noqa: F401,F403
+from .operators import *  # noqa: F401,F403
